491f7ad050975762c3af4c6bd35517dea1b0ff2b
nargizmzade <nmammadzada8966@ada.edu.az>
2026-04-05 23:22:56 +0400

    update

--- Stadium.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-public class Stadium : BaseEntity
-{
-    public string Name { get; set; }
-    public int Id { get; set; }
-}
--- Team.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class Team :BaseEntity
-{
-    public string Name { get; set; }
-    public int Id { get; set; }
-    public int StadiumId { get; set; }
-    public Stadium? Stadium { get; set; }
-}
